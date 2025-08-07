c953ea09c812589362e207c8c783456537c92e55
Jeremy Nyangi <justinjerome401@gmail.com>
2025-08-07 20:38:45 +0300

    Fix href

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,13 +1,13 @@
 'use client'
- 
+
 import Logo from '@/components/logo'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import Button from "@/components/button";
- 
+
 export default function NavBar() {
     const router = useRouter()
- 
+
     const navItems = [
         {
             text: 'Find a car',
@@ -30,28 +30,27 @@ export default function NavBar() {
             href: '#',
         },
     ]
- 
+
     return (
         <header className="bg-primary-100 py-4">
             <nav aria-label="Global">
                 <div className="mx-auto max-w-8xl p-spacing flex items-center justify-between">
-                    <a href='/'>
+                    <Link href="/" className="flex items-center">
                         <span className='flex md:hidden'>
                             <Logo text={false} />
                         </span>
                         <span className='hidden md:flex'>
                             <Logo />
                         </span>
-                    </a>
+                    </Link>
                     <ul className='flex flex-wrap items-center justify-center gap-8 lg:gap-12'>
-                            {navItems.map((item, index) => (
-                                <li key={index}>
-                                    <Link
-                                        href={item.href}>
-                                        {item.text}
-                                    </Link>
-                                </li>
-                            ))}
+                        {navItems.map((item, index) => (
+                            <li key={index}>
+                                <Link href={item.href}>
+                                    {item.text}
+                                </Link>
+                            </li>
+                        ))}
                     </ul>
                 </div>
             </nav>
